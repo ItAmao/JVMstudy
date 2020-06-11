5330c29e8f9e920ad88c15fac5c27e602f63ef2c
ItAmao <452861727@qq.com>
2020-06-11 14:56:44 +0800

    JVM第一天学习

---
+++ src/com/amao/LearnJVM/chapter01/demo01_前言/Test.java
@@ -0,0 +1,10 @@
+package com.amao.LearnJVM.chapter01.demo01_前言;
+
+/**
+ * @author mao
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("起一个");
+    }
+}
