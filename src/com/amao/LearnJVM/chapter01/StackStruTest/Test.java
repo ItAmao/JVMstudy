6695eeacaca58a76f5fe29fb7e0310d0839026ca
ItAmao <452861727@qq.com>
2020-06-13 18:16:21 +0800

    JVM 第一章笔记 6月13号 星期六

---
+++ src/com/amao/LearnJVM/chapter01/StackStruTest/Test.java
@@ -0,0 +1,14 @@
+package com.amao.LearnJVM.chapter01.StackStruTest;
+
+/**
+ * @author mao
+ */
+public class Test {
+
+    public static void main(String[] args) {
+        int i = 2 + 3;
+        //进入out进行反编译
+        //反编译指令
+        //进入 文件  javap -V + 字节码文件的名称
+    }
+}
